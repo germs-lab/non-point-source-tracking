190b9d971949e8446127301ac1ccfc387a4850ba
adina <adina.chuang@gmail.com>
2014-11-21 16:06:15 -0600

    Update download.sh

--- data/download.sh
+++ data/download.sh
@@ -1,8 +1,8 @@
-#/root/sratoolkit.2.4.2-ubuntu64/bin/fastq-dump SRR401333
-#/root/sratoolkit.2.4.2-ubuntu64/bin/fastq-dump SRR401334
-#/root/sratoolkit.2.4.2-ubuntu64/bin/fastq-dump SRR401335
-#mkdir 16S-data
-#mv *fastq 16S-data/
+#These are the 16S 454 datasets for Lake Lanier.  The adapters and barcodes had to be cleaned.  Only reads with appropriate alignment with the primers stated in teh paper were selected.
+/root/sratoolkit.2.4.2-ubuntu64/bin/fastq-dump SRR401333
+/root/sratoolkit.2.4.2-ubuntu64/bin/fastq-dump SRR401334
+/root/sratoolkit.2.4.2-ubuntu64/bin/fastq-dump SRR401335
+
 #/root/sratoolkit.2.4.2-ubuntu64/bin/fastq-dump SRR096389
 #sewage
-root/sratoolkit.2.4.2-ubuntu64/bin/fastq-dump SRP041262
+#root/sratoolkit.2.4.2-ubuntu64/bin/fastq-dump SRP041262
