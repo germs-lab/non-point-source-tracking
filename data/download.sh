f43d65e4b6ea2c86af326010df9a50b74986efeb
root <root@howe-base.novalocal>
2014-11-21 22:04:08 +0000

    added download SRA

---
+++ data/download.sh
@@ -0,0 +1,8 @@
+#/root/sratoolkit.2.4.2-ubuntu64/bin/fastq-dump SRR401333
+#/root/sratoolkit.2.4.2-ubuntu64/bin/fastq-dump SRR401334
+#/root/sratoolkit.2.4.2-ubuntu64/bin/fastq-dump SRR401335
+#mkdir 16S-data
+#mv *fastq 16S-data/
+#/root/sratoolkit.2.4.2-ubuntu64/bin/fastq-dump SRR096389
+#sewage
+root/sratoolkit.2.4.2-ubuntu64/bin/fastq-dump SRP041262
